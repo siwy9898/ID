bc21d914e57ba91fa47bc37940045c78ad406862
Kuba_IBM <Kuba_IBM@192.168.0.136>
2021-11-18 21:02:10 +0100

    *

--- src/tablice/dwuwymiarowe/Main.java
+++ src/tablice/dwuwymiarowe/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//		sprawedzam jak działa pomiedzy kompami
+//		kocham Ole
 //		int[][] macierz = new int[5][6];
 //		int[][] macierz2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9, 6 } };
 //		// ilosc wierszy
